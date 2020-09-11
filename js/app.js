3bf3d35fca1a42d6d4bb478080dca86e8ab8535c
SieChan <sie.chan@outlook.com>
2020-09-10 21:09:22 -0600

    edited the getTweets function

--- js/app.js
+++ js/app.js
@@ -91,3 +91,41 @@ function deleteTweet() {
 }
 
 deleteTweet();
+
+//GET all the tweets
+
+function getTweets() {
+    let tweetTitle = document.getElementById("title-input").value;
+    let tweetBody = document.getElementById("body-input").value;
+
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    };
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let posts = JSON.parse(this.responseText);
+            console.log(posts);
+
+            for (i = 0; i < posts.length; i++) {
+                let allTweets = document.getElementById("show-tweets");
+                allTweets.innerHTML = " ";
+
+            }
+        } else if (this.readyState != 4) {
+            console.log("LOADING...");
+        } else {
+            console.log("Something went wrong here!");
+        }
+    }
+
+
+    ajax.open("GET", "https://jsonplaceholder.typicode.com/posts", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData));
+}
+
+getTweets();
