b326bf87b9f6b74a4ef56d78588563dc166deacb
SieChan <sie.chan@outlook.com>
2020-09-10 20:44:06 -0600

    made functions for send,update and deleting the tweets

---
+++ js/app.js
@@ -0,0 +1,93 @@
+//Post tweet
+function sendTweet() {
+    let tweetTitle = document.getElementById("title-input").value;
+    let tweetBody = document.getElementById("body-input").value;
+
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    };
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 201) {
+            console.log(JSON.parse(this.responseText));
+            alert("Your Tweet has been Submitted!")
+        } else if (this.readyState != 4) {
+            console.log("LOADING...")
+        } else {
+            console.log("Something went wrong!");
+            console.log(this.status);
+        }
+    }
+
+    ajax.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData));
+}
+
+let tweetButton = document.getElementById("tweet-submit");
+tweetButton.addEventListener("click", sendTweet);
+
+//PATCH = update the tweet
+function patchTweet() {
+    let tweetTitle = document.getElementById("title-input").value;
+    let tweetBody = document.getElementById("body-input").value;
+
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    };
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+
+        } else if (this.readyState != 4) {
+            console.log("LOADING...")
+        } else {
+            console.log("Something went wrong!");
+            console.log(this.status);
+        }
+    }
+
+    ajax.open("PATCH", "https://jsonplaceholder.typicode.com/posts/1", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send(JSON.stringify(tweetData))
+}
+
+patchTweet();
+
+//DELETE Data
+
+function deleteTweet() {
+    let tweetTitle = document.getElementById("title-input").value;
+    let tweetBody = document.getElementById("body-input").value;
+
+    let tweetData = {
+        title: tweetTitle,
+        body: tweetBody,
+        userId: 1
+    };
+
+    let ajax = new XMLHttpRequest();
+    ajax.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+
+        } else if (this.readyState != 4) {
+            console.log("LOADING...")
+        } else {
+            console.log("Something went wrong!");
+            console.log(this.status);
+        }
+    }
+    ajax.open("DELETE", "https://jsonplaceholder.typicode.com/posts/1", true);
+    ajax.setRequestHeader("Content-Type", "application/json");
+    ajax.send();
+}
+
+deleteTweet();
